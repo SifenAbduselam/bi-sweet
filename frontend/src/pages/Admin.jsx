c4a2327e7540f7e1fcda3cac1fd3758004f6aaf8
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-07-04 03:15:25 -0700

    updating admin

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,17 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
 export default function Admin() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-      navigate("/admin/login");
-    }
-  }, []);
-
   function logout() {
     localStorage.removeItem("token");
     window.location.href = "/admin/login";
