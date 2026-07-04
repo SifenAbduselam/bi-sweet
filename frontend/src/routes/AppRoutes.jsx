eb88e041cfd5e1e04127ed4847264a082406266f
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-07-04 03:04:55 -0700

    using protected route for admin.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+import ProtectedRoute from "../components/ProtectedRoute";
 import Home from "../pages/Home";
 import Booking from "../pages/Booking";
 import Admin from "../pages/Admin";
@@ -10,7 +10,13 @@ export default function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/booking" element={<Booking />} />
 <Route path="/admin/login" element={<Login />} />    
-  <Route path="/admin" element={<Admin />} />
-    </Routes>
+<Route
+  path="/admin"
+  element={
+    <ProtectedRoute>
+      <Admin />
+    </ProtectedRoute>
+  }
+/>    </Routes>
   );
 }
